7e31a0d1e17036396ce858c13fd61d065b66c77f
Artem Bulkhak <artem.bulkhak@mail.ru>
2024-07-09 11:34:54 +0300

    git init

---
+++ src/main/java/com/artembulkhak/tinkoffinvestanalysis/TinkoffInvestAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.artembulkhak.tinkoffinvestanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinkoffInvestAnalysisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TinkoffInvestAnalysisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/artembulkhak/tinkoffinvestanalysis/TinkoffInvestAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artembulkhak.tinkoffinvestanalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinkoffInvestAnalysisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
